e4c5cd793c019bdd33addb87f2296a0c4dda058e
jotace8 <juancasega1@gmail.com>
2022-02-25 19:14:07 +0100

    as

--- web_sstt.py
+++ web_sstt.py
@@ -96,6 +96,15 @@ def process_web_request(cs, webroot):
             * Si es por timeout, se cierra el socket tras el período de persistencia.
                 * NOTA: Si hay algún error, enviar una respuesta de error con una pequeña página HTML que informe del error.
     """
+
+    while(True):
+        data  = recibir_mensaje(cs)
+        print(data)
+
+        cerrar_conexion(cs)
+
+    sys.exit
+
     while(True):
         data = recibir_mensaje(cs)
         enviar_mensaje(cs, data)
