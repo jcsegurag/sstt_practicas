922b92543fdf5a5bf7c293f5e559733e49c2ad52
jotace8 <juancasega1@gmail.com>
2022-03-12 19:27:12 +0100

    asdc

--- web_sstt.py
+++ web_sstt.py
@@ -137,7 +137,7 @@ def process_web_request(cs, webroot):
             header = "HTTP/1.1 405 Method Not Allowed\r\n" + "Date: " + str(datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT\r\n')) + "Server: iotforyou03.org\r\n" + "Content-Length: " + str(os.stat("./405.html").st_size) + "\r\n" + "Connection: Connection Close\r\n" + "Content-Type: " + content + "\r\n\r\n" 
             tam5 = os.stat("./405.html").st_size
             enviar_recurso(ruta, header, tam5, cs)
-            cerrar_conexion(cs)
+            #cerrar_conexion(cs)
 
         # Comprobar si la versión de HTTP es 1.1
         if(lineas_solicitud[2] != "HTTP/1.1"):
