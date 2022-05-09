db91adc8605809aeae14750ba918c68f17975a52
jotace8 <juancasega1@gmail.com>
2022-05-10 01:49:42 +0200

    zxczxc

--- web_sstt.py
+++ web_sstt.py
@@ -174,7 +174,7 @@ def process_web_request(cs, webroot):
         text = ""
         res = re.compile(formato).fullmatch(lineas[0])
         text = res.group(2)[1:]
-        print(res)
+        print(text)
         recurso = text
         recurso = text.split(sep='?', maxsplit=1)[0]
 
